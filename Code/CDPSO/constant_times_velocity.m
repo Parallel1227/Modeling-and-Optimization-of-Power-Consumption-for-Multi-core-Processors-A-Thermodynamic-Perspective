0e5156de845ab7cd5b2b1f81498d7291a4dee258
Parallel1227 <141417575+Parallel1227@users.noreply.github.com>
2024-12-31 15:14:23 +0800

    Delete Code directory

--- Code/CDPSO/constant_times_velocity.m
+++
@@ -1,9 +0,0 @@
-function change = constant_times_velocity(constant,change)
-for i=1:size(change,1)
-    for j=1:size(change,2)
-        if rand>constant
-            change(i,j)=0;
-        end
-    end
-end
-end
